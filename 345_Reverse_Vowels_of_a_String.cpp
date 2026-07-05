31b3454d68e8fe15b4735cc4d70c2acbd22ebaa4
WilliamBill <bill939252@gmail.com>
2026-07-05 11:40:33 +0800

    using two pointer

--- 345_Reverse_Vowels_of_a_String.cpp
+++ 345_Reverse_Vowels_of_a_String.cpp
@@ -21,8 +21,30 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    string reverseVowels(string s) {
+        int l = 0;
+        int r = s.size() - 1;
+        while(l < r){
+            if((tolower(s[l]) == 'a' || tolower(s[l]) == 'e' || tolower(s[l]) == 'i' || tolower(s[l]) == 'o' || tolower(s[l]) == 'u') && (tolower(s[r]) == 'a' || tolower(s[r]) == 'e' || tolower(s[r]) == 'i' || tolower(s[r]) == 'o' || tolower(s[r]) == 'u')){
+                swap(s[l],s[r]);
+                l ++;
+                r --;
+            }
+            else if(tolower(s[r]) != 'a' && tolower(s[r]) != 'e' && tolower(s[r]) != 'i' && tolower(s[r]) != 'o' && tolower(s[r]) != 'u') r--;
+            else if(tolower(s[l]) != 'a' && tolower(s[l]) != 'e' && tolower(s[l]) != 'i' && tolower(s[l]) != 'o' && tolower(s[l]) != 'u') l++;
+            else {
+                r--;
+                l++;
+            }
+        }
+        // cout << s;
+        return s;
+    }
+};
 int main(){
-    Solution solution;
-    string s = "IceCreAm";
+    Solution2 solution;
+    string s = "leetcode";
     solution.reverseVowels(s);
 }
