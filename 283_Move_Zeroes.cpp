3400d0f46fa067fc0d3e5e052ee6de6f2f37a98e
WilliamBill <bill939252@gmail.com>
2026-07-10 00:10:00 +0800

    keep all non-zero elements at beginning, and fill the remaining positions with zereos.

--- 283_Move_Zeroes.cpp
+++ 283_Move_Zeroes.cpp
@@ -21,9 +21,28 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+        int k = 0;
+
+        for(int num : nums){
+
+            if(num != 0){
+                nums[k++] = num;
+            }
+        }
+
+        while(k < nums.size()){
+            nums[k++] = 0;
+        }
+    }
+};
+
 
 int main(){
-    Solution solution;
+    Solution2 solution;
     vector<int> nums = {0,1,0,3,12};
     solution.moveZeroes(nums);
 }
