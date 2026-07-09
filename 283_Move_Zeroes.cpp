1afa0f1d02c3339ea89670f761d54d32384f2ef3
WilliamBill <bill939252@gmail.com>
2026-07-10 00:04:56 +0800

    move zeroes(change the original's thinking method.)

--- 283_Move_Zeroes.cpp
+++ 283_Move_Zeroes.cpp
@@ -5,21 +5,19 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int l = 0;
-        int r = nums.size()-1;
-        while(r > 0){
-            if(nums[l] == 0 && nums[r] == 0){
-                l++;
-                r--;
+        while(l < nums.size()){
+            if(nums[l] == 0){
+                for(int i = l; i< nums.size(); i++){
+                    if(nums[i] != 0){
+                        swap(nums[l],nums[i]);
+                        break;
+                    }
+                }
             }
-            else if(nums[l] == 0 && nums[r] != 0){
-                swap(nums[l],nums[r]);
-                l++;
-                r--;
-            }
-            else{
-                l++;
-            }
+            l++;
+            
         }
+
     }
 };
 
