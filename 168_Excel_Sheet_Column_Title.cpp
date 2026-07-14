beea844da90295fccfbf41c5b790172579e2151c
WilliamBill <bill939252@gmail.com>
2026-07-14 23:56:10 +0800

    You can decrement columnNuber by 1 to simplify your implementation

--- 168_Excel_Sheet_Column_Title.cpp
+++ 168_Excel_Sheet_Column_Title.cpp
@@ -22,6 +22,25 @@ public:
     }
 };
 
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string ans;
+
+        while (columnNumber > 0) {
+            columnNumber--;
+
+            ans += char('A' + columnNumber % 26);
+
+            columnNumber /= 26;
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
+
 int main(){
     Solution solution;
     int columnNumber = 701;
