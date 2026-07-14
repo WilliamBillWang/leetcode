37ba69c368ec173b706f02d4117ad2bb97217393
WilliamBill <bill939252@gmail.com>
2026-07-14 10:58:48 +0800

    Can't use pointer or you will lost the information.

--- 682_Baseball_Game.cpp
+++ 682_Baseball_Game.cpp
@@ -34,8 +34,50 @@ public:
     }
 };
 
+class Solution2 {//can't use the pointer
+public:
+    int calPoints(vector<string>& operations) {
+        int* fir = nullptr;
+        int* second = nullptr;
+        int sum = 0;
+        for(string str : operations){
+            if(str == "C"){
+                sum -= *fir;
+                fir = second;
+            }
+            else if(str == "D"){
+                sum += (*fir) * 2;
+            }
+            else if(str == "+"){
+                sum += (*fir) + (*second);
+            }
+            else{
+                if(fir == nullptr) {
+                    int val = stoi(str);
+                    fir = &val;
+                    sum += val;
+                }
+                else if(second == nullptr){
+                    int val = stoi(str);
+                    second = &val;
+                    sum += val;
+                }
+                else{
+                    int temp = stoi(str);
+                    second = fir;
+                    fir = &temp;
+                    sum += temp;
+                }
+            }
+            
+        }
+        return sum;
+    }
+};  
+
+
 int main(){
-    Solution solution;
+    Solution2 solution;
     vector<string> operations = {"5","-2","4","C","D","9","+","+"};
     solution.calPoints(operations);
 }
