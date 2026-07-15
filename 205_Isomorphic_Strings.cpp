f16fe8bed5f435348cff2c673a319209717a8f6f
WilliamBill <bill939252@gmail.com>
2026-07-15 15:25:00 +0800

    Using double map by vector

--- 205_Isomorphic_Strings.cpp
+++ 205_Isomorphic_Strings.cpp
@@ -22,8 +22,28 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    bool isIsomorphic(string s, string t) {
+        vector<char> s_to_t(128,0);
+        vector<char> t_to_s(128,0);
+
+        for(int i =0; i< s.size() ;i ++){
+            char ch1 = s[i];
+            char ch2 = t[i];
+
+            if(s_to_t[ch1] && s_to_t[ch1] != ch2) return false;
+            if(t_to_s[ch2] && t_to_s[ch2] != ch1) return false;
+
+            s_to_t[ch1] = ch2;
+            t_to_s[ch2] = ch1;
+        }
+        return true;
+    }
+};
+
 int main(){
-    Solution solution;
-    string s = "badc", t = "baba";
+    Solution2 solution;
+    string s = "egg", t = "add";
     solution.isIsomorphic(s,t);
 }
