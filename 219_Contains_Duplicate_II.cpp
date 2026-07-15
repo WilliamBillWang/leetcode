81f18ddda3a7ef1b7c41630d3df6000073f139c3
WilliamBill <bill939252@gmail.com>
2026-07-15 17:00:17 +0800

    practice

--- 219_Contains_Duplicate_II.cpp
+++ 219_Contains_Duplicate_II.cpp
@@ -18,39 +18,77 @@ public:
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int, int> mp;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (mp.count(nums[i])) {
-                if (i - mp[nums[i]] <= k)
-                    return true;
+        unordered_map<int,int> mp;
+        for(int i =0; i< nums.size() ; i++){
+            if(mp.count(nums[i])){
+                if(i - mp[nums[i]] <= k) return true;
             }
-
             mp[nums[i]] = i;
         }
-
         return false;
     }
 };
+
+
+// class Solution {
+// public:
+//     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+//         unordered_map<int, int> mp;
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (mp.count(nums[i])) {
+//                 if (i - mp[nums[i]] <= k)
+//                     return true;
+//             }
+
+//             mp[nums[i]] = i;
+//         }
+
+//         return false;
+//     }
+// };
 
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_set<int> window;
+        unordered_set<int> st;
+        for(int i =0; i< nums.size() ; i ++){
+            if(st.count(nums[i])) return true;
+            st.insert(nums[i]);
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (window.count(nums[i]))
-                return true;
-
-            window.insert(nums[i]);
-
-            if (window.size() > k)
-                window.erase(nums[i - k]);
+           if(st.size() > k) st.erase(nums[i - k]);
         }
-
         return false;
     }
 };
+
+
+
+
+
+
+// class Solution {
+// public:
+//     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+//         unordered_set<int> window;
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (window.count(nums[i]))
+//                 return true;
+
+//             window.insert(nums[i]);
+
+//             if (window.size() > k)
+//                 window.erase(nums[i - k]);
+//         }
+
+//         return false;
+//     }
+// };
+
+
+
+
 
 
 int main(){
