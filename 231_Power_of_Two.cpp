b07144cdbc97410af05dabcb6593415b9fdb1b72
WilliamBill <bill939252@gmail.com>
2026-07-16 12:12:59 +0800

    Using bit manipulation

--- 231_Power_of_Two.cpp
+++ 231_Power_of_Two.cpp
@@ -13,6 +13,29 @@ public:
     }
 };
 
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+
+        if(n <= 0) return false;
+        while(n > 0){
+            if(n == 1) return true;
+            if(n % 2 != 0) return false;
+            n /= 2;
+        }
+        return true;
+    }
+};
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+         return (n > 0) && !(n&(n-1));
+    }
+};
+
+
 int main(){
     for(int i =0; i< 50; i++){
         cout << pow(2,i) << ",";
