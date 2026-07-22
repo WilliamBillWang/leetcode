3b8195da6fda8be4e53ad4e7783a7c4562a5b8e5
WilliamBill <bill939252@gmail.com>
2026-07-22 23:26:58 +0800

    Using only one map

--- 350_Intersection_of_Two_Array_II.cpp
+++ 350_Intersection_of_Two_Array_II.cpp
@@ -36,6 +36,31 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+
+        if (nums1.size() > nums2.size())
+            swap(nums1, nums2);
+
+        unordered_map<int,int> cnt;
+
+        for (int x : nums1)
+            cnt[x]++;
+
+        vector<int> ans;
+
+        for (int x : nums2) {
+            if (cnt[x] > 0) {
+                ans.push_back(x);
+                cnt[x]--;
+            }
+        }
+
+        return ans;
+    }
+};
+
 int main(){
     vector<int> nums1 = {1,2,2,1};
     vector<int> nums2 = {2,2};
