edaf020d0473b693f174b97d92914646d32d0ee9
WilliamBill <bill939252@gmail.com>
2026-07-22 22:42:56 +0800

    Power of three(Accepted on the first attempt)

---
+++ 326_Power_of_three.cpp
@@ -0,0 +1,21 @@
+using namespace std;
+#include<bits/stdc++.h>
+
+    class Solution {
+    public:
+        bool isPowerOfThree(int n) {
+            if(n <= 0) return false;
+            while(n > 1) {
+                if(n % 3 != 0) return false;
+                n/=3;
+            }
+            if(n == 1) return true;
+            return false;
+        }
+    };
+
+int main(){
+    Solution solution;
+    int n = 27;
+    solution.isPowerOfThree(n);
+}
